be27837c5e19b13e91b7ae7da0a2992beba04341
myy <myy@qq.com>
2021-06-16 19:19:48 +0800

    master first commit!

---
+++ module01_github/src/main/java/com/ybxymyy/GitTest.java
@@ -0,0 +1,14 @@
+package com.ybxymyy;
+
+public class GitTest {
+    //写代码
+    public static void main(String[] args) {
+        System.out.println("hello git! hello myy!!！");
+        System.out.println("hello git! hello world!");
+        System.out.println("hello git! hello xll !!!");
+        System.out.println("这是使用idea集成git的hot-fix分支！！！");
+        System.out.println("测试合并冲突的master代码！！！");
+        System.out.println("测试合并冲突的hot-fix代码！！！");
+        System.out.println("今天是2021-6-16");
+    }
+}
